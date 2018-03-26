33e840ef4f70f2af807061ba1ac82426267e4676
sarun <sarundahal11@gmail.com>
2018-03-26 13:50:08 +0545

    Deletes a script setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(name='ALPRsystem',
-      version='0.1',
-      description='ALPR system using ANN',
-      options = {"build_exe": {"packages": ["cv2", "numpy", "decimal", "os", "shutil","wx","pickle","scipy"], "include_files": []}},
-      executables= [Executable("main.py", base = "Win32GUI")])
-
